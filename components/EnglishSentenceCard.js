3cf71e05b921facb086276be496cc9211d42c554
Gusty <a0985209465@gmail.com>
2023-12-16 02:03:41 +0800

    增加句子切換、換loading圖片

--- components/EnglishSentenceCard.js
+++ components/EnglishSentenceCard.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { StyleSheet, View, ActivityIndicator } from 'react-native'
-import { Card } from '@rneui/themed'
+import { Card, Button } from '@rneui/themed'
+import { EvilIcons } from '@expo/vector-icons'
 
 import MyText from './MyText'
 import { baseGetInstance } from '../tools/MyAxiosInstance'
@@ -9,9 +10,12 @@ import Styles from '../constants/Styles'
 /*
   首頁的英文句子組件
 */
-export default function EnglishSentenceCard () {
+export default function EnglishSentenceCard() {
   const [engSentence, setEngSentence] = useState('')
+  const [sentenceChange, setSentenceChange] = useState(false)
+
   useEffect(() => {
+    //query英文句子
     baseGetInstance
       .get(
         'https://gusty1.github.io/Database/cramForInterview/englishSentence.json'
@@ -21,10 +25,21 @@ export default function EnglishSentenceCard () {
         let randomNumber = Math.floor(Math.random() * sentenceResultSet.length)
         setEngSentence(sentenceResultSet[randomNumber])
       })
-  }, [])
+  }, [sentenceChange])
+
+  //刷新英文句子
+  function refreshSentence() {
+    setSentenceChange(!sentenceChange)
+  }
 
   return (
     <Card containerStyle={styles.englishCard}>
+      <View style={styles.sentenceContainer}>
+        <MyText style={styles.sentenceTitle}>每日一句:</MyText>
+        <Button size="sm" type="clear" onPress={refreshSentence}>
+          <EvilIcons name="refresh" size={25} color="#0080FF" />
+        </Button>
+      </View>
       {engSentence === '' ? (
         <ActivityIndicator size="large" />
       ) : (
@@ -40,7 +55,7 @@ export default function EnglishSentenceCard () {
 const styles = StyleSheet.create({
   englishCard: {
     ...Styles.defaultBorder,
-    ...Styles.defaultShadow
+    ...Styles.defaultShadow,
   },
   sentenceStyle: {
     fontSize: 18,
@@ -49,5 +64,11 @@ const styles = StyleSheet.create({
     color: '#5B5B5B',
     textAlign: 'right',
     paddingTop: 5,
+  },
+  sentenceContainer: {
+    flexDirection: 'row',
+  },
+  sentenceTitle: {
+    fontSize: 16,
   },
 })
